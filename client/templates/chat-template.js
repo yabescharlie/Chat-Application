c0bccaadfc30c6d49caea21ba22d3f87b4717fbf
MarkVJIII <MarkVJ19@gmail.com>
2015-07-09 19:41:48 +0800

    Initialize Project First Commit

---
+++ client/templates/chat-template.js
@@ -0,0 +1,57 @@
+Template.appChat.helpers({
+	messages: function (){
+			return Messages.find({}, {sort: {createdAt: -1}, limit: 10})
+	}
+
+
+});
+
+Template.appChat.events({
+	"click .btnDelete": function (event){
+		Messages.remove(this._id);
+		return false;
+	},
+	"keyup .txtText": function (event){
+		if(event.keyCode == 13){
+			{
+				if(!!$(".txtText ").val() && !!$(".txtName ").val() )
+				{
+
+	
+			Messages.insert({
+				name: $(".txtName").val(),
+				text: $(".txtText").val(),
+				createdAt: new Date()
+			});
+			 $(".txtText").val("");
+			 $(".txtText").focus();
+			}
+		}
+		}
+
+
+		return false;
+	},
+	"keyup .txtName": function (event){
+		if(event.keyCode == 13){
+			{
+				if(!!$(".txtText ").val() && !!$(".txtName ").val() )
+				{
+
+	
+			Messages.insert({
+				name: $(".txtName").val(),
+				text: $(".txtText").val(),
+				createdAt: new Date()
+			});
+			 $(".txtText").val("");
+			 $(".txtText").focus();
+			}
+		}
+		}
+
+
+		return false;
+	}
+
+});
